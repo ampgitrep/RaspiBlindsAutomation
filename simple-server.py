69a11a9a2e4bcf5c8f16d573a5c9c086f8dc5066
ap <apulvers@my.chemeketa.edu>
2021-12-11 23:20:56 +0000

    added bulma CDN to webpage.html. added routing to webpage, and return function to webpage so the server doesn't return to root directory after each reload

--- simple-server.py
+++ simple-server.py
@@ -7,49 +7,35 @@ from time import sleep
 from datetime import datetime
 import pytz
 
-
-
-
-
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
+        if self.path == '/':
+          self.path = 'webpage.html'
+          return http.server.SimpleHTTPRequestHandler.do_GET(self)
+                
         query_components = parse_qs(urlparse(self.path).query) 
-        next_name = 0
-        if 'increment' in query_components:
-         next_name = next(myiter)
-        elif 'move_motors_up' in query_components:
+        
+        if 'move_motors_up' in query_components:
           move_motors_up()
+          self.path = 'webpage.html'
+          return http.server.SimpleHTTPRequestHandler.do_GET(self)
         elif 'move_motors_down' in query_components:
-          move_motors_down() 
-        html = f"<html><head></head><body> <form method=get action=?increment>Hello {list[next_name]} <input type=hidden name=increment value=true><input type=submit value=Increment></input></form> <form method=get action=?move_motors_up>Move blinds up! <input type=hidden name=move_motors_up value=true><input type=submit value=up></input></form> <form method=get action=?move_motors_down>Move blinds down! <input type=hidden name=move_motors_down value=true><input type=submit value=down></input></form></body></html>"
+          move_motors_down()
+          self.path = 'webpage.html'
+          return http.server.SimpleHTTPRequestHandler.do_GET(self)
+  
+        html = f"<html><head></head><body><form method=get action=?move_motors_up>Move blinds up! <input type=hidden name=move_motors_up value=true><input type=submit value=up></input></form><form method=get action=?move_motors_down>Move blinds down! <input type=hidden name=move_motors_down value=true><input type=submit value=down></input></form></body></html>"
         self.wfile.write(bytes(html, "utf8"))
           
-        
         return 
-list = ["alex", "phoenix", "jack", "lucy"]
+
 
 tz_LA = pytz.timezone('America/Los_Angeles')
 now = datetime.now(tz_LA)
 current_time = now.strftime("%H:%M:%S")
-
-class increment_index():
-  def __iter__(self):
-    self.index = 0
-    return self
-  
-  def __next__(self):
-    next_index = self.index
-    if next_index < len(list):
-      self.index += 1
-    else:
-      self.index = 0
-      next_index = self.index
-    return next_index
-myclass = increment_index()
-myiter = iter(myclass)
 
 def move_motors_up():
   GPIO.setmode(GPIO.BOARD)
@@ -63,10 +49,7 @@ def move_motors_up():
     sleep(1)
     GPIO.output(8, False)
     pwm.ChangeDutyCycle(0)
-
-  
   SetAngle(90)
-  
   pwm.stop()
   GPIO.cleanup()
   
@@ -82,8 +65,6 @@ def move_motors_down():
     sleep(1)
     GPIO.output(8, False)
     pwm.ChangeDutyCycle(0)
-
-  
   SetAngle(10)
   pwm.stop()
   GPIO.cleanup()
